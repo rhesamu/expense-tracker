4e527dd1c156fef01bfa099e9b1ceeed22d3f7a9
Rhesa Utomo <venderblitz@gmail.com>
2018-05-25 00:56:06 +0700

    added report filtering

--- routes/report.js
+++ routes/report.js
@@ -1,12 +1,66 @@
 const router = require('express').Router();
+const models = require('../models');
+const UserExpense = models.UserExpense;
+const Expense = models.Expense;
 
 router.get('/', (req, res) => {
   // res.send('test report page')
-  res.render('report')
+  UserExpense.findAll({
+    attributes: [ 
+      'id', 
+      'userId', 
+      'expenseId', 
+      'amount', 
+      'createdAt', 
+      'updatedAt' ],
+    order: [['createdAt', 'DESC']],
+    where: { userId: 2 }, //req.session.userId
+    include: [{
+      model: Expense
+    }]
+  })
+  .then(result => {
+    // res.json(result)
+    let qs = req.query
+    console.log(qs)
+    res.render('report', { userExpense: result })
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err.message)
+  })
 })
 
 router.post('/', (req, res) => {
-  res.send('test report page')
+  let month = req.body.month; //01-12
+  // res.send(month)
+  UserExpense.findAll({
+    attributes: [ 
+      'id', 
+      'userId', 
+      'expenseId', 
+      'amount', 
+      'createdAt', 
+      'updatedAt' ],
+    order: [['createdAt', 'DESC']],
+    where: { 
+      userId: 2,
+      createdAt: {
+        $between: [
+          `2018-${month}-01T05:16:21.078Z`,
+          `2018-${month}-30T05:16:21.078Z`
+        ]
+      }
+    },
+    include: [{ model: Expense }]
+  })
+  .then(result => {
+    res.render('report', { userExpense: result })
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err.message)
+  })
 })
 
 module.exports = router;
